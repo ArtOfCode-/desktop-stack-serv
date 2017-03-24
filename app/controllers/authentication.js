f0306279a60d91c16522a067aad8ea51b9fcb064
ArtOfCode <owen.jenkins2903@gmail.com>
2017-03-24 13:05:41 +0000

    Update nails version

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -2,40 +2,44 @@ var url = require('url');
 
 var accessTokens = {};
 
-exports.redirect = function (req, render) {
+exports.index = function () {
+  this.redirect('http://stackapps.com/q/7271/29659');
+};
+
+exports.redirect = function (req) {
   var uri = url.parse(req.url, true);
   if (uri.query.error && uri.query.error === "true") {
-    render({view: true, locals: {redirect: false, success: false}});
+    this.render({view: true, locals: {redirect: false, success: false}});
   }
   else if (!uri.query.access_token && !uri.query.state) {
-    render({view: true, locals: {redirect: true, success: false}});
+    this.render({view: true, locals: {redirect: true, success: false}});
   }
   else {
     var state = uri.query.state;
     var token = uri.query.access_token;
     if (state && token) {
       accessTokens[state] = token;
-      render({view: true, locals: {redirect: false, success: true}});
+      this.render({view: true, locals: {redirect: false, success: true}});
     }
     else {
-      render({view: true, locals: {redirect: false, success: false}});
+      this.render({view: true, locals: {redirect: false, success: false}});
     }
   }
 };
 
-exports.token = function (req, render) {
+exports.token = function (req) {
   var uri = url.parse(req.url, true);
   if (uri.query.key) {
     var token = accessTokens[uri.query.key];
     if (token) {
       delete accessTokens[uri.query.key];
-      render({json: {access_token: token}});
+      this.render({json: {access_token: token}});
     }
     else {
-      render({json: {error_name: 'no_token', error_message: 'No token was found for the given key'}, status: 404});
+      this.render({json: {error_name: 'no_token', error_message: 'No token was found for the given key'}, status: 404});
     }
   }
   else {
-    render({json: {error_name: 'no_key', error_message: 'No key was provided to fetch token with'}, status: 400});
+    this.render({json: {error_name: 'no_key', error_message: 'No key was provided to fetch token with'}, status: 400});
   }
 };
